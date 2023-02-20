23dfda80d69a49c82fdc20ea2fd88cf9847688f6
dipubhandari <dipendrabhandari123456@gmail.com>
2023-02-19 18:40:33 -0800

    validation added

--- controller/bookingController.js
+++ controller/bookingController.js
@@ -43,37 +43,46 @@ class BookingController {
         }
     }
     // suscrible......................
-
     // BOOKING API
     static Booking = async (req, res) => {
         try {
             // getting data from user
-            const { fullname, email, secondemail, phone, city, country, tripname, travellers, day, tripdate, message } = req.body
+            const { fullname, email, phone, city, country, tripname, travellers, day, tripdate, message } = req.body
+            const secondemail = req.body.secondemail || 'null'
             // validating the input
             if (!(fullname && email && phone && city && country && tripname && travellers && day && tripdate && message)) {
                 res.send({ error_msg: "Please Enter the details" })
             }
+            else if (phone.toString().length != 10 || typeof phone != 'number') {
+                res.send({ error_msg: "phone number is not valid" })
+            }
             else {
-                if (phone.length < 10 || phone.length > 10 || typeof phone != 'number') {
-                    res.send({ error_msg: "Phone number is not valid" })
-                }
+
                 const validateEmail = uservalidator.validate(email)
-                if (!(validateEmail)) {
+                let validateEmailII
+                if (secondemail != 'null') {
+                    validateEmailII = uservalidator.validate(secondemail)
+                }
+                else {
+                    validateEmailII = true
+                }
+                if ((validateEmail == false || validateEmailII == false)) {
                     res.send({ error_msg: "Please send correct Email" })
                 }
-                // send email
-                let transporter = nodemailer.createTransport({
-                    service: 'gmail',
-                    auth: {
-                        user: 'realdipubhandari@gmail.com',
-                        pass: 'vrbosdgmhoolidon'
-                    }
-                });
-                let mailOptions = {
-                    from: '<Nomad Server>',
-                    to: '<realdipubhandari@gmail.com>',
-                    subject: 'Booking...',
-                    html: `
+                else {
+                    // send email
+                    let transporter = nodemailer.createTransport({
+                        service: 'gmail',
+                        auth: {
+                            user: 'realdipubhandari@gmail.com',
+                            pass: 'vrbosdgmhoolidon'
+                        }
+                    });
+                    let mailOptions = {
+                        from: '<Nomad Server>',
+                        to: '<realdipubhandari@gmail.com>',
+                        subject: 'Booking...',
+                        html: `
                     <b>Hello Someone wants to book trip Here is the details....</b>
                      <br />
                      <br />
@@ -81,7 +90,7 @@ class BookingController {
                   <b>Name :</b> <p>${fullname}</p>  <br />
                   <b>email : <p>${email}</p>  <br />
                   <b>Secondary Email : <p>${secondemail}</p>  <br />
-                  <b>Phone :</b> <p>${phone}</p>  <br />
+                  <b>phone :</b> <p>${phone}</p>  <br />
                   <b>Address-Ciry :</b> <p>${city}</p>  <br />
                   <b>Country :</b> <p>${country}</p>  <br />
                   <b>Trip Date :</b> <p>${tripdate.split(['T'][0])}</p>  <br />
@@ -90,24 +99,19 @@ class BookingController {
                   <b>Total Days : </b><p>${day}</p>  <br />
                   <b>Message :</b> <p>${message}</p>  <br />
                     `
-                };
-                transporter.sendMail(mailOptions, function (error, info) {
-                    if (error) {
-                        res.send(error)
-                    } else {
-                        console.log('Email sent: ' + info.response);
+                    };
+                    transporter.sendMail(mailOptions, function (error, info) {
+                    });
+                    // 
+                    const save = await BookingModel.create({ fullname, email, secondemail, phone, city, country, tripname, travellers, day, tripdate, message })
+                    if (save) {
+                        res.send({ success_msg: "Booking is Successful..." })
                     }
-                });
-                // 
-                const save = await BookingModel.create({ fullname, email, secondemail, phone, city, country, tripname, travellers, day, tripdate, message })
-                if (save) {
-                    res.send({ success_msg: "Booking is Successful..." })
-                }
-                else {
-                    res.send({ error_msg: "Unable to book" })
+                    else {
+                        res.send({ error_msg: "Unable to book" })
+                    }
                 }
             }
-
         } catch (error) {
             res.send({ error_msg: "Something went wrong" })
         }
@@ -118,33 +122,37 @@ class BookingController {
     // inquiry
 
     static Inquiry = async (req, res) => {
-        // getting the inpu
-        const { tripname, fullname, email, phone, message } = req.body
-        // validting the input
-        if (!(tripname && fullname && email && phone && message)) {
-            res.send({ error_msg: "Please enter all fields" })
-        }
 
-        else {
-            try {
+        try {
+            // getting the inpu
+            const { tripname, fullname, email, phone, message } = req.body
+            // validting the input
+            if (!(tripname && fullname && email && phone && message)) {
+                console.log(tripname, fullname, email, phone, message)
+                res.send({ error_msg: "Please enter all fields" })
+            }
+            else if (typeof phone != 'number' || phone.toString().length != 10) {
+                res.send({ error_msg: "phone number is not valid" })
+            }
+            else {
+                // try {
                 const validateEmail = uservalidator.validate(email)
                 if (!validateEmail) {
                     res.send({ error_msg: " Email is not valid" })
                 }
-
-                // send email
-                let transporter = nodemailer.createTransport({
-                    service: 'gmail',
-                    auth: {
-                        user: 'realdipubhandari@gmail.com',
-                        pass: 'vrbosdgmhoolidon'
-                    }
-                });
-                let mailOptions = {
-                    from: '<Nomad Server>',
-                    to: '<realdipubhandari@gmail.com>',
-                    subject: 'Booking...',
-                    html: `
+                else {
+                    let transporter = nodemailer.createTransport({
+                        service: 'gmail',
+                        auth: {
+                            user: 'realdipubhandari@gmail.com',
+                            pass: 'vrbosdgmhoolidon'
+                        }
+                    });
+                    let mailOptions = {
+                        from: '<Nomad Server>',
+                        to: '<realdipubhandari@gmail.com>',
+                        subject: 'Booking...',
+                        html: `
                         <b>Hello Someone wants to inquiry on Nomad....</b> <br />
                         Here is the details
                          <br />
@@ -152,23 +160,22 @@ class BookingController {
                 <b>Trip name:<b/>  ${tripname}  <br />
                         <b> Name:<b/> ${fullname}  <br />
                         <b>Email:<b/>${email} <br />
-                        <b>Phone:<b/>${phone}
+                        <b>phone:<b/>${phone}
                         <b>Message:<b/>${message}
                                              
                         `
-                };
-                transporter.sendMail(mailOptions, function (error, info) {
-                    if (error) {
-                        res.send(error)
+                    };
+                    transporter.sendMail(mailOptions, function (error, info) {
+                    })
+                    const newInquiry = await InquiryModel.create({ tripname, fullname, email, phone, message })
+                    if (newInquiry) {
+                        res.send({ success_msg: "We will Contact you Soon..." })
                     }
-                })
-                const newInquiry = await InquiryModel.create({ tripname, fullname, email, phone, message })
-                if (newInquiry) {
-                    res.send({ success_msg: "We will Contact you Soon..." })
                 }
-            } catch (error) {
-                console.log(res.send({ error_msg: "Something Went Wrong" }))
             }
+        }
+        catch (err) {
+            res.send({ error_msg: "Something Went Wrong" })
         }
     }
 
@@ -180,55 +187,54 @@ class BookingController {
         try {
             // getting input
             let { name, email, phone, country, findus, traveldate, message } = req.body
-            const numberType = typeof phone
             if (!(name && email && phone && country && findus && traveldate && message)) {
                 res.send({ error_msg: "Please fill all the details" })
             }
-            if ((phone.length !== 10) || typeof phone != 'number') {
-                res.status(200).send({ error_msg: "Phone number is not valid" })
+            else if (typeof phone != 'number' || phone.toString().length != 10) {
+                res.status(200).send({ error_msg: "phone number is not valid" })
             }
             else {
-                
-                console.log(typeof phone)
                 const validateEmail = uservalidator.validate(email)
                 if (!(validateEmail)) {
                     res.send({ error_msg: "Please send correct Email" })
                 }
-                const contact = await Contact_Model.create({
-                    name, email, phone, country, findus, traveldate, message
-                })
-                if (contact) {
-                    // send email
-                    let transporter = nodemailer.createTransport({
-                        service: 'gmail',
-                        auth: {
-                            user: 'realdipubhandari@gmail.com',
-                            pass: 'vrbosdgmhoolidon'
-                        }
+                else {
+                    const contact = await Contact_Model.create({
+                        name, email, phone, country, findus, traveldate, message
                     })
-                    let mailOptions = {
-                        from: '<Nomad Server>',
-                        to: '<realdipubhandari@gmail.com>',
-                        subject: 'Booking...',
-                        html: `
+                    if (contact) {
+                        res.send({ success_msg: "We will contact soon!" })
+                        // send email
+                        let transporter = nodemailer.createTransport({
+                            service: 'gmail',
+                            auth: {
+                                user: 'realdipubhandari@gmail.com',
+                                pass: 'vrbosdgmhoolidon'
+                            }
+                        })
+                        let mailOptions = {
+                            from: '<Nomad Server>',
+                            to: '<realdipubhandari@gmail.com>',
+                            subject: 'Booking...',
+                            html: `
                         Hello, ${name} wants to contact on Nomad.... <br />
                         Here is the details <br /> <br />
                        <b>Email:<b/>  ${email} <br /> <br />
-                        <b> Phone:<b/> ${phone}  <br /> <br />
+                        <b> phone:<b/> ${phone}  <br /> <br />
                         <b>Country:<b/>${country} <br /> <br />
                         <b>Find You By:<b/>${findus} <br /> <br />
                         <b>Travel Date:<b/>${traveldate.split(['T'][0])} <br /> <br />
                         <b>Message:<b/>${message}
                         `
-                    };
-                    transporter.sendMail(mailOptions, function (error, info) {
-                        if (error) {
-                            res.send(error)
-                        }
-                    })
-                    res.send({ success_msg: "We will contact soon!" })
+                        };
+                        transporter.sendMail(mailOptions, function (error, info) {
+
+                        })
+
+                    }
                 }
             }
+
             // validating input
         } catch (error) {
             res.send({ error_msg: "Something Went Wrong" })
